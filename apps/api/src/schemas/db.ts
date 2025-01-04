c0e1ba4303fb08ec08ce3d2f5c37fc5e6e1bbb48
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-01-04 21:05:00 +0530

    feat: add db schemas

--- apps/api/src/schemas/db.ts
+++ apps/api/src/schemas/db.ts
@@ -1,10 +1,26 @@
-import { json, pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { bigint, json, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('user', {
   id: serial('id').primaryKey(),
   email: varchar('email', { length: 255 }).unique().notNull(),
-  emailVerified: timestamp('emailVerified', { mode: 'date' }),
-  user_metadata: json().$type<{ name: string; avatar_url: string | null }>().notNull(),
+  emailVerified: timestamp('email_verified', { mode: 'date' }),
+  metadata: json().$type<{ name: string; avatar_url: string | null }>().notNull(),
+  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'date' }),
+});
+
+export const accountTable = pgTable('account', {
+  id: serial('id').primaryKey(),
+  userId: bigint('user_id', { mode: 'number' })
+    .references(() => userTable.id, { onDelete: 'cascade' })
+    .notNull(),
+  accountId: bigint('account_id', { mode: 'number' }).notNull(),
+  providerId: varchar('provider_id', { length: 255 }).notNull(),
+  refreshToken: text('refresh_token'),
+  refreshTokenExpiresAt: timestamp('created_at', { mode: 'date' }),
+  scope: text('scope'),
+  idToken: text('id_token'),
+  metadata: json().$type<{ password: string | null }>().notNull(),
   createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
   updatedAt: timestamp('updated_at', { mode: 'date' }),
 });
---
+++ apps/api/src/services/auth/types.ts
@@ -0,0 +1,5 @@
+import type { User } from '@/api/schemas/types';
+
+export interface Auth {
+  user: User;
+}
