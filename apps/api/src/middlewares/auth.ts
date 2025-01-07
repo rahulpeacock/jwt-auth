a50752a38dfd2232e04ed2d27a7d20984596b2d6
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-01-07 18:14:19 +0530

    feat: add auth middleware

--- apps/api/src/middlewares/auth.ts
+++ apps/api/src/middlewares/auth.ts
@@ -1,19 +1,22 @@
+import { getCookie } from 'hono/cookie';
 import { createMiddleware } from 'hono/factory';
+import * as HttpStatusCodes from 'stoker/http-status-codes';
+import { env } from '../lib/env';
+import { verifyJwtToken } from '../services/auth/jwt';
 import type { Auth } from '../services/auth/types';
 
-export const authMiddleware = createMiddleware<{ Variables: { auth: Auth } }>((c, next) => {
-  c.set('auth', {
-    user: {
-      id: 1,
-      email: 't2oYK@example.com',
-      emailVerified: null,
-      metadata: {
-        name: 'John Doe',
-        avatar_url: null,
-      },
-      createdAt: new Date(),
-      updatedAt: null,
-    },
-  });
-  return next();
+export const authMiddleware = createMiddleware<{ Variables: { auth: Auth } }>(async (c, next) => {
+  const accessToken = getCookie(c, 'jwt-auth.access_token', 'secure');
+
+  if (!accessToken) {
+    return c.json({ message: 'User unauthorized', code: 'LOGIN' }, HttpStatusCodes.UNAUTHORIZED);
+  }
+
+  try {
+    const decodedPayload = await verifyJwtToken(accessToken, env.ACCESS_TOKEN);
+    c.set('auth', decodedPayload);
+    return next();
+  } catch (err) {
+    return c.json({ message: 'User unauthorized', code: 'LOGIN' }, HttpStatusCodes.UNAUTHORIZED);
+  }
 });
